3af3208db2931c992df92ff7d3a812cc06d67c48
rohan77ash <140048299+rohan77ash@users.noreply.github.com>
2025-07-27 22:48:41 +0530

    Create 05_risk_score.sql

---
+++ snowflake/05_risk_score.sql
@@ -0,0 +1,28 @@
+create or replace table cfpb.data.company_risk_scores as
+with last_90_days as (
+  select
+    company,
+    state,
+    complaint_date,
+    complaints_count,
+    spike_percent
+  from cfpb.data.complaint_spikes
+  where complaint_date >= dateadd(day, -90, current_date)
+)
+select
+  company,
+  state,
+  avg(spike_percent) as avg_spike_percent,
+  sum(complaints_count) as total_complaints_90d,
+  case
+    when avg(spike_percent) > 50 then 50
+    when avg(spike_percent) > 25 then 30
+    else 10
+  end +
+  case
+    when sum(complaints_count) > 500 then 30
+    when sum(complaints_count) > 200 then 20
+    else 10
+  end as risk_score
+from last_90_days
+group by company, state;
