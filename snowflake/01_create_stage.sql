eca0206b975065f75e77d1fee892bc1db9c35f81
rohan77ash <140048299+rohan77ash@users.noreply.github.com>
2025-07-27 14:03:05 +0530

    Create 01_create_stage.sql

---
+++ snowflake/01_create_stage.sql
@@ -0,0 +1,11 @@
+create or replace storage integration si_blob
+  type = external_stage
+  storage_provider = 'azure'
+  enabled = true
+  azure_tenant_id = '12345678-90ab-cdef-1234-567890abcdef'
+  storage_allowed_locations = ('azure://cfpbdatastorage.blob.core.windows.net/landing-ext-stage/raw');
+
+create or replace stage raw_cfpb_ext_stage
+  url = 'azure://cfpbdatastorage.blob.core.windows.net/landing-ext-stage/raw'
+  storage_integration = my_blob_integration
+  file_format = (type = 'json');
