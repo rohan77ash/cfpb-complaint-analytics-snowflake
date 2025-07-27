324d03b245d16c45232584f72d04b775cff23696
rohan77ash <140048299+rohan77ash@users.noreply.github.com>
2025-07-27 19:43:32 +0530

    Update 01_table_load.sql

--- snowflake/01_table_load.sql
+++ snowflake/01_table_load.sql
@@ -1,8 +1,25 @@
-create or replace table raw_cfpb_complaints (
-  raw variant
+create or replace table cfpb.data.raw_cfpb_complaints (
+  date_received DATE,
+  product STRING,
+  sub_product STRING,
+  issue STRING,
+  sub_issue STRING,
+  narrative STRING,
+  company_response STRING,
+  company STRING,
+  state STRING,
+  zip_code STRING,
+  tags STRING,
+  consumer_consent STRING,
+  submitted_via STRING,
+  date_sent_to_company DATE,
+  company_response_to_consumer STRING,
+  timely_response STRING,
+  consumer_disputed STRING,
+  complaint_id STRING
 );
 
 copy into raw_cfpb_complaints
 from @raw_cfpb_stage
-file_format = (type = 'JSON');
+file_format = (type = 'CSV');
 
