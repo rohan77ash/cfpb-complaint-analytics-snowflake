1a300325f5952f354aa7803b5bb8d9887748e3a0
rohan77ash <140048299+rohan77ash@users.noreply.github.com>
2025-07-27 22:31:56 +0530

    Update 02_table_load.sql

--- snowflake/02_table_load.sql
+++ snowflake/02_table_load.sql
@@ -2,8 +2,12 @@ CREATE OR REPLACE TABLE raw_cfpb_complaints_json (
   raw VARIANT
 );
 
-copy into raw_cfpb_complaints_json
-from @raw_cfpb_stage
-file_format = (type = 'JSON');
+create or replace pipe my_snowpipe 
+as
+copy into raw_cfpb_complaints
+from @my_blob_stage
+file_format = (type = 'json');
+
+
 
 
