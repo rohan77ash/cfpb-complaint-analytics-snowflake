23a9f0f111709a82f073e5e4a8d502143fb98a1e
rohan77ash <140048299+rohan77ash@users.noreply.github.com>
2025-08-15 23:10:41 +0530

    Update 05_views.sql

--- snowflake/05_views.sql
+++ snowflake/05_views.sql
@@ -4,6 +4,7 @@ select * from cfpb.data.complaint_daily_summary;
 create or replace view cfpb.data.v_complaint_spikes as
 select * from cfpb.data.complaint_spikes;
 
+
 create or replace view v_company_risk_scores as
 select *
 from company_risk_scores
